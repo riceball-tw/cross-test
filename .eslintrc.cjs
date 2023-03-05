40237d3994a156ba5d76bfb7a23ecbafac2d5cd5
Riceball <42161586+Anderice@users.noreply.github.com>
2023-03-05 15:51:55 +0800

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="App">
+            <h1 className="bg-red-300 text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
+}
